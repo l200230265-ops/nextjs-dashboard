f98d9dcbafaa3f41ab5eaa65a1ef84466510b917
l200230265 <l200230265@student.ums.ac.id>
2025-12-20 03:56:44 +0700

    bab 14

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -21,10 +21,10 @@ export default function EditInvoiceForm({
 }) {
   const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+  const [state, dispatch] = useActionState(updateInvoiceWithId, initialState);
  
   return (
-    <form action={formAction}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
