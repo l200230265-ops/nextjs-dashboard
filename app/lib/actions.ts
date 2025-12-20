1c371b0e873b00f096fd8a8126238321d0e55763
l200230265 <l200230265@student.ums.ac.id>
2025-12-20 23:15:19 +0700

    perbaikan 3

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,7 +7,7 @@ import postgres from 'postgres';
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.postgres_POSTGRES_URL!, { ssl: 'require' });
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -10,12 +10,10 @@ import {
 import { formatCurrency } from './utils';
 
 console.log("Koneksi ke:", process.env.POSTGRES_URL ? "Ditemukan" : "KOSONG!");
-const sql = postgres(process.env.POSTGRES_URL_NON_POOLING!, { ssl: 'require' });
+const sql = postgres(process.env.postgres_POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
 
      console.log('Fetching revenue data...');
      await new Promise((resolve) => setTimeout(resolve, 3000));
@@ -53,9 +51,7 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
-    // You can probably combine these into a single SQL query
-    // However, we are intentionally splitting them to demonstrate
-    // how to initialize multiple queries in parallel with JS.
+
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
